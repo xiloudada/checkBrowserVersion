07ae86853caf7d9d2a84aeef79abbcb9d77b9a75
xilou <shenggongqiang@163.com>
2023-12-15 14:52:15 +0800

    feat: check browser version

--- app/page.tsx
+++ app/page.tsx
@@ -1,94 +1,63 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+'use client'
+import {useEffect, useState} from "react";
+import VConsole from "vconsole";
+
 
 export default function Home() {
+    const [value, setValue] = useState('')
+
+    useEffect(() => {
+        const vConsole = new VConsole()
+        const isServer = typeof window === 'undefined'
+        const userAgent = navigator.userAgent;
+        if (!isServer && userAgent){
+            // 判断浏览器类型和版本
+            if (userAgent.indexOf("Chrome") != -1) {
+                // Chrome
+                // @ts-ignore
+                const chromeVersion = userAgent.match(/Chrome\/(\d+\.\d+)/)[1];
+                console.log("Chrome 版本：" + chromeVersion);
+                setValue("Chrome 版本：" + chromeVersion)
+            } else if (userAgent.indexOf("Firefox") != -1) {
+                // Firefox
+                // @ts-ignore
+                const firefoxVersion = userAgent.match(/Firefox\/(\d+\.\d+)/)[1];
+                console.log("Firefox 版本：" + firefoxVersion);
+                setValue("Firefox 版本：" + firefoxVersion)
+            } else if (userAgent.indexOf("Safari") != -1) {
+                // Safari
+                // @ts-ignore
+                const safariVersion = userAgent.match(/Version\/(\d+\.\d+)/)[1];
+                console.log("Safari 版本：" + safariVersion);
+                setValue("Safari 版本：" + safariVersion)
+            } else if (userAgent.indexOf("MSIE") != -1 || userAgent.indexOf("Trident") != -1) {
+                // Internet Explorer
+                // @ts-ignore
+                const ieVersion = userAgent.match(/(?:MSIE |Trident\/.*; rv:)(\d+\.\d+)/)[1];
+                console.log("IE 版本：" + ieVersion);
+                setValue("IE 版本：" + ieVersion)
+            } else if (userAgent.indexOf("Edge") != -1) {
+                // Microsoft Edge
+                // @ts-ignore
+                const edgeVersion = userAgent.match(/Edge\/(\d+\.\d+)/)[1];
+                console.log("Edge 版本：" + edgeVersion);
+                setValue("Edge 版本：" + edgeVersion)
+            } else {
+                // 其他浏览器
+                console.log("无法识别的浏览器");
+                setValue("无法识别的浏览器")
+            }
+        }
+
+        return () =>{
+            vConsole.destroy()
+        }
+    })
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+    <main>
+      <div style={{width:'100vw',height:'100vh',overflow:'auto',display:'flex',alignItems:'center',justifyContent:'center'}}>
+        你的浏览器版本是：{value}
       </div>
     </main>
   )
